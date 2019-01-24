8cbeb3e51da76c1a4ad4c43b7e8ac96a7e100696
Daniel Corcoran <danielc.92@outlook.com>
2019-01-24 11:02:55 +1100

    Applied pep8 formtting to app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-
+"""Imports."""
 from flask import Flask
 from flask import request
 from flask import url_for
@@ -19,8 +19,10 @@ db = SQLAlchemy(app)
 
 # Database Models
 
+
 class Robots(db.Model):
-    """Maps to robots table"""
+    """Maps to robots table."""
+
     __tablename__ = 'robots'
 
     robot_id = db.Column(db.Float, primary_key=True)
@@ -30,10 +32,13 @@ class Robots(db.Model):
     robot_joined = db.Column(db.Text)
 
     def __repr__(self):
+        """Return record string."""
         return '<robots %r>' % self.robot_id
 
+
 class Threads(db.Model):
-    """Maps to threads table"""
+    """Maps to threads table."""
+
     __tablename__ = 'threads'
 
     thread_id = db.Column(db.Float, primary_key=True)
@@ -45,10 +50,13 @@ class Threads(db.Model):
     thread_stars = db.Column(db.Float)
 
     def __repr__(self):
+        """Return record string."""
         return '<threads %r>' % self.thread_id
 
+
 class Comments(db.Model):
-    """Maps to comments table"""
+    """Maps to comments table."""
+
     __tablename__ = 'comments'
 
     comment_id = db.Column(db.Float, primary_key=True)
@@ -59,6 +67,7 @@ class Comments(db.Model):
     comment_thumbs_down = db.Column(db.Float)
 
     def __repr__(self):
+        """Return record string."""
         return '<comments %r>' % self.comment_id
 
 # Create alias for ORM classes
@@ -68,9 +77,10 @@ R = Robots
 
 # Helper Functions
 
+
 def fetch_side_data():
-    """Fetch right hand side bar data from database and return dictionary"""
-    limit=10
+    """Fetch right hand side bar data from database and return dictionary."""
+    limit = 10
 
     starred_threads = T.query.order_by(desc(T.thread_stars)).limit(limit)
     new_members = R.query.order_by(desc(R.robot_joined)).limit(limit)
@@ -82,54 +92,66 @@ def fetch_side_data():
 
     return sidebar_data
 
-# Routes
 
 @app.route('/')
 def home():
+    """Route to view news, the home page."""
     return render_template('home.html', side_data=fetch_side_data())
+
 
 @app.route('/members/')
 def members():
+    """Route to view a multiple members."""
     members = R.query.all()
     return render_template('members.html', members=members, side_data=fetch_side_data())
 
+
 @app.route('/threads/')
 def threads():
-    threads = T.query.join(R, R.robot_id == T.thread_robot_id).add_columns(R.robot_name, 
-        T.thread_name, 
-        T.thread_stars,
-        T.thread_id,
-        T.thread_date,
-        T.thread_tags,
-        T.thread_content).all()
+    """Route to view multiple threads."""
+    threads = T.query.join(R, R.robot_id == T.thread_robot_id)\
+                     .add_columns(R.robot_name,
+                                  T.thread_name,
+                                  T.thread_stars,
+                                  T.thread_id,
+                                  T.thread_date,
+                                  T.thread_tags,
+                                  T.thread_content).all()
     return render_template('threads.html', threads=threads, side_data=fetch_side_data())
+
 
 @app.route('/threads/view/')
 def threadview():
-
+    """Route to view a single thread."""
     thread_id = request.args.get('thread_id')
 
-    thread = T.query.join(R, R.robot_id == T.thread_robot_id).add_columns(R.robot_name,
-        R.robot_avatar, 
-        T.thread_name, 
-        T.thread_stars,
-        T.thread_id,
-        T.thread_date,
-        T.thread_tags,
-        T.thread_content).filter(T.thread_id==thread_id).first()
+    thread = T.query.join(R, R.robot_id == T.thread_robot_id)\
+                    .add_columns(R.robot_name,
+                                 R.robot_avatar,
+                                 T.thread_name,
+                                 T.thread_stars,
+                                 T.thread_id,
+                                 T.thread_date,
+                                 T.thread_tags,
+                                 T.thread_content)\
+                    .filter(T.thread_id == thread_id).first()
     return render_template('thread-view.html', thread=thread, side_data=fetch_side_data())
+
 
 @app.route('/members/view/')
 def memberview():
-
+    """Route to view a single member."""
     member_id = request.args.get('member_id')
 
     member = R.query.filter(R.robot_id == member_id).first()
     return render_template('member-view.html', member=member, side_data=fetch_side_data())
 
 # Test Routes - for checking query results
+
+
 @app.route('/test-query/')
 def test():
+    """Test queries."""
     robots = R.query.all()
     threads = T.query.all()
     comments = C.query.all()
@@ -138,9 +160,14 @@ def test():
     print(comments)
     return '<h1>Test Route</h1>'
 
+
 @app.route('/test-join/')
 def testjoin():
-    query = C.query.join(R, R.robot_id == T.thread_robot_id).add_columns(R.robot_name).order_by(C.comment_id).limit(10)
+    """Test join queries."""
+    query = C.query.join(R, R.robot_id == T.thread_robot_id)\
+                   .add_columns(R.robot_name)\
+                   .order_by(C.comment_id).limit(10)
+
     for q in query:
         print(q.robot_name)
     return '<h1>Test Join Route</h1>'
